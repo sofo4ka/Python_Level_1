648cdfa361c06c69143bfd0011284124d24dda3d
sofo4ka <33701267+sofo4ka@users.noreply.github.com>
2017-11-19 00:10:18 +0300

    Update and rename hw04_easy.py to Lesson_4/hw04_easy.py

--- Lesson_4/hw04_easy.py
+++ Lesson_4/hw04_easy.py
@@ -1,4 +1,5 @@
 __author__ = 'Шварева Софья Дмитриевна'
+
 # Все задачи текущего блока решите с помощью генераторов списков!
 
 print('Задание-1: ')
@@ -8,10 +9,12 @@ print('Задание-1: ')
 # [1, 2, 4, 0] --> [1, 4, 16, 0]
 
 from random import randint
+
 random_list = [randint(-100, 100) for _ in range(10)]
 list_of_squares = [x*x for x in random_list]
 print(random_list)
 print(list_of_squares)
+
 
 print('Задание-2:')
 # Даны два списка фруктов.
@@ -21,6 +24,7 @@ fruits1 = ['яблоко', 'банан', 'манго', 'персик', 'анан
 fruits2 = ['манго', 'апельсин', 'банан',  'ананас']
 fruits3 = [fruit for fruit in fruits1 if fruit in fruits2]
 print(fruits3)
+
 
 print('Задание-3:')
 # Дан список, заполненный произвольными числами.
